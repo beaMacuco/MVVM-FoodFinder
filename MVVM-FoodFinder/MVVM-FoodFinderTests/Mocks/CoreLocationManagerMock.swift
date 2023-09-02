fea7d2b0ba5b037f6ee693b7ee6d8c5a9fafb19e
BeaMacuco <beatriz.louresmacuoc@gmail.com>
2023-09-02 05:37:50 +0200

    core location mock

--- MVVM-FoodFinder/MVVM-FoodFinderTests/Mocks/CoreLocationManagerMock.swift
+++ MVVM-FoodFinder/MVVM-FoodFinderTests/Mocks/CoreLocationManagerMock.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import CoreLocation
+
+enum LocationErrorMock: Error {
+    case failedToSendUpdate
+}
+
+class CoreLocationManagerMock: CoreLocationManager {
+    var authorizationRequested: Bool = false
+    
+    var locationRequested: Bool = false
+    
+    var locationManagerDelegate: CoreLocationManagerDelegate?
+    
+    var pausesLocationUpdatesAutomatically: Bool = false
+    
+    var desiredAccuracy: CLLocationAccuracy = .nan
+    
+    var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    
+    func requestWhenInUseAuthorization() {
+        authorizationRequested = true
+    }
+    
+    func requestLocation() {
+        locationRequested = true
+    }
+}
