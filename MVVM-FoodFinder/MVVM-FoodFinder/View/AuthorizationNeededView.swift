eb028db3620e51d5912f5f3bc803915b8148b96e
BeaMacuco <beatriz.louresmacuoc@gmail.com>
2023-09-02 05:39:14 +0200

    location authorization handling view

--- MVVM-FoodFinder/MVVM-FoodFinder/View/AuthorizationNeededView.swift
+++ MVVM-FoodFinder/MVVM-FoodFinder/View/AuthorizationNeededView.swift
@@ -5,4 +5,23 @@
 //  Created by Beatriz Loures Macuco on 16/08/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct AuthorizationNeededView: View {
+    private let settingsUrl = UIApplication.settingsUrl
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: StaticViewValues.ViewSpacing.largeVerticalView) {
+            Text(StaticViewValues.LocalisableStrings.greeting)
+            Text(StaticViewValues.LocalisableStrings.authorizationNeededMessage)
+                .font(.callout)
+                .foregroundStyle(.gray)
+            
+            if let settingsUrl =  settingsUrl {
+                Link(StaticViewValues.LocalisableStrings.goToSettings, destination: settingsUrl)
+                    .foregroundStyle(.blue)
+            }
+        }
+        .padding()
+    }
+}
